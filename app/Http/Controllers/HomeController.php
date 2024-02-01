f8ca6f491d73a0dfd99a350c2a1a1c141703480b
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-02-01 14:44:16 +0700

    bai 3 bai 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,15 @@ class HomeController extends Controller
     public function index(){
         return view('/home');
     }
+
+    public function get_name($name){
+        if(isset($_GET['name'])){
+            echo $_GET['name'];
+        }else{
+            echo $name;
+        }
+    }
 }
+
+// $home = new HomeController();
+// $home->get_name();
--- resources/views/contact/test2.blade.php
+++ resources/views/contact/test2.blade.php
@@ -1,5 +1,5 @@
 <h1>test2</h1>
-<form action="/home" method="post">
+<form action="/home" method="get">
     <input type="text" name="name">
     <input type="text" name="_method" value="get">
     <input type="text" name="_token" value="<?php echo csrf_token(); ?>">
